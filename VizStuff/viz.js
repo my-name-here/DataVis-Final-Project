99bcede2aa1831305d1737685a5490335f87b8e4
CB4747 <123422381+CB4747@users.noreply.github.com>
2025-04-19 11:16:01 -0700

    set the viz to show in the js

--- VizStuff/viz.js
+++ VizStuff/viz.js
@@ -1,33 +1,13 @@
 //button click reference
 //https://www.w3schools.com/jsref/event_onclick.asp
 
-var fileIndex = 0;
-const fileList = ["viz1.html", "viz2.html", "viz3.html", "viz4.html"];
 
-function constrainVal(minVal, maxVal, val){
-    return Math.max(minVal, Math.min(maxVal, val)) // using clever min and max function usage, can constrain it between the given values
-}
+const Frame1 = document.getElementById("viz1");
+const Frame2 = document.getElementById("viz2");
+const Frame3 = document.getElementById("viz3");
+const Frame4 = document.getElementById("viz4");
 
-const upButton = document.getElementById("UpButton");
-const downButton = document.getElementById("DownButton");
-
-const Frame = document.getElementById("vizFrame");
-
-function changeIframe(){
-    // don't forget that when we change it on button presses, we need to constrain it
-    Frame.src = "Visualizations/" + fileList[fileIndex]
-}
-
-//based on button click reference(https://www.w3schools.com/jsref/event_onclick.asp)
-upButton.onclick = function(){
-    fileIndex = constrainVal(0,3, fileIndex - 1);
-    changeIframe()
-};
-
-//based on button click reference(https://www.w3schools.com/jsref/event_onclick.asp)
-downButton.onclick = function(){
-    fileIndex = constrainVal(0,3, fileIndex + 1);
-    changeIframe()
-};
-
-changeIframe()
+Frame1.src = "viz1.html";
+Frame2.src = "viz2.html";
+Frame3.src = "viz3.html";
+Frame4.src = "viz4.html";
