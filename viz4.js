2ada2010c50191443b6852f30195ab2c2ff85ab0
CB4747 <123422381+CB4747@users.noreply.github.com>
2025-04-29 15:10:15 -0700

    remove variably conflicts

--- viz4.js
+++ viz4.js
@@ -1,16 +1,14 @@
 // basic framework from class example, edited to work for my needs
 // started with a copy of my bar chart, and edited
 // Set up the SVG container
-const svgWidth = 1000;
-const svgHeight = 600;
-const margin = { top: 50, right: 220, bottom: 350, left: 150 };
-const width = svgWidth - margin.left - margin.right;
-const height = svgHeight - margin.top - margin.bottom;
+const hourlySvgWidth = 1000;
+const hourlySvgHeight = 600;
+const hourlyMargin = { top: 50, right: 220, bottom: 350, left: 150 };
+const hourlyWidth = hourlySvgWidth - hourlyMargin.left - hourlyMargin.right;
+const hourlyHeight = hourlySvgHeight - hourlyMargin.top - hourlyMargin.bottom;
 let choices = ["Bayview Hunters Point", "Financial District/South Beach", "Mission", "South of Market", "Tenderloin"]
 let hourOptionsList = ["00", "01", "02", "03", "04", "05", "06", "07", "08", "09", "10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
-const minSize = 1
-const maxSize = 6
-let years
+
 
 
 function getNextHour(CurHour){
@@ -24,13 +22,13 @@ function getNextHour(CurHour){
 
 }
 
-const svg = d3.select("#chart-container")
+const hourlySvg = d3.select(".chart-container-hourly")
     .append("svg")
     // using viewbox instead of width and height since viewbox makes responsive (see https://stackoverflow.com/a/63156174
-    .attr("viewBox", `0 0 ${svgWidth} ${svgHeight}`)
+    .attr("viewBox", `0 0 ${hourlySvgWidth} ${hourlySvgHeight}`)
     
     .append("g")
-    .attr("transform", `translate(${margin.left},${margin.top})`);
+    .attr("transform", `translate(${hourlyMargin.left},${hourlyMargin.top})`);
 
 // a function that takes a neighborhood, and gives out either the neighborhood if it is in the list of saved neighborhoods, or other
 function locRange(i){
@@ -116,13 +114,13 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
     //see https://d3js.org/d3-scale/band
     const y = d3.scaleBand()
         .domain( choices)
-        .range([ 0, -height])
+        .range([ 0, -hourlyHeight])
         //.padding(0.1);
 
     // scale band instead of time, since it reduces annoyance later hopefully
     const x = d3.scaleBand()
         .domain(hourOptionsList)
-        .range([ 0, width]);
+        .range([ 0, hourlyWidth]);
     
     
     var colorScale = d3.scaleSequential()
@@ -135,15 +133,15 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
 
     // Add X and Y axes
     
-    svg.append("g")
+    hourlySvg.append("g")
         .attr("class", "axis axis-x")
         // ${x(d3.timeParse("%y")(72)-d3.timeParse("%y")(71))/2} shifts axis label for year to middle of year box
-        .attr("transform", `translate(0, ${height})`)
+        .attr("transform", `translate(0, ${hourlyHeight})`)
         .call(d3.axisBottom(x).ticks(24));
 
-    svg.append("g")
+    hourlySvg.append("g")
         .attr("class", "axis axis-y")
-        .attr("transform", `translate(0, ${height})`)
+        .attr("transform", `translate(0, ${hourlyHeight})`)
         .call(d3.axisLeft(y).ticks(20));
 
     // Add bars
@@ -168,7 +166,7 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
         .attr("class", "tooltip")
 
 
-    bars =  svg.selectAll(".bar")
+    bars =  hourlySvg.selectAll(".bar")
         .data(dataSpots)
         .enter()
         .append("g")
@@ -184,7 +182,7 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
         .attr("stroke-width", 1)
         .attr("stroke", "black")
         .attr("fill", d=>colorScale(hours.get(d[0]).get(d[1])))
-        .attr("transform", `translate(0, ${height})`)// translate points down to match with axis
+        .attr("transform", `translate(0, ${hourlyHeight})`)// translate points down to match with axis
         // needs to be event,d, so that the value of d is passed in along with the mouse event
         .on("mouseover", function(event, d){
             
@@ -228,7 +226,7 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
         
             type: d3.annotationCalloutLabel,
             x: x("18")+x.step()/2,
-            y: height+y("Mission")+y.step()/2,
+            y: hourlyHeight+y("Mission")+y.step()/2,
             dx: 10,
             dy:130,
             color: "black"
@@ -241,7 +239,7 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
         
             type: d3.annotationCalloutLabel,
             x: x("15")+x.step()/2,
-            y: height+y("Tenderloin")+y.step()/2,
+            y: hourlyHeight+y("Tenderloin")+y.step()/2,
             dx: -35,
             dy:130+2*y.step(),
             color: "black"
@@ -254,28 +252,28 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
         .annotations(annotations)
     d3.select("svg")
         .append("g")
-        .attr("transform", ` translate(${margin.left},${margin.top}) `)
+        .attr("transform", ` translate(${hourlyMargin.left},${hourlyMargin.top}) `)
         .call(makeAnnotations);
   
 
-    svg.append("text")
+    hourlySvg.append("text")
         .attr("class","axisLabelY")
         .text("neighborhood")
-        .attr("x", -margin.left+10)
-        .attr("y", height/2)
+        .attr("x", -hourlyMargin.left+10)
+        .attr("y", hourlyHeight/2)
         
-    svg.append("text")
+    hourlySvg.append("text")
         .attr("class","axisLabelX")
         .text("hour")
-        .attr("x", width/2)
-        .attr("y", height+50)
+        .attr("x", hourlyWidth/2)
+        .attr("y", hourlyHeight+50)
 
-    svg.append("text")
+    hourlySvg.append("text")
     
         .text("heatmap of the accidents each hour for different neighborhoods")
         .attr("class", "title")
-        .attr("x", (width-margin.left)/2-margin.right)
-        .attr("y", -margin.top/2)
+        .attr("x", (hourlyWidth-hourlyMargin.left)/2-hourlyMargin.right)
+        .attr("y", -hourlyMargin.top/2)
     var legend = d3.legendColor()
 		.title("Color Legend: Number of Crashes")
 		.titleWidth(100)
@@ -283,7 +281,7 @@ d3.csv("https://raw.githubusercontent.com/my-name-here/DataVis-Final-Project/ref
         .scale(colorScale);
 		
 
-    svg.append("g")
-        .attr("transform", `translate(${width+margin.right/2+20},0)`)
+    hourlySvg.append("g")
+        .attr("transform", `translate(${hourlyWidth+hourlyMargin.right/2+20},0)`)
         .call(legend);
 });
