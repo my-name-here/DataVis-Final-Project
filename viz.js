0fafa1cfa7e01eb7ff6b3a56e67e286b79ec445f
CB4747 <123422381+CB4747@users.noreply.github.com>
2025-04-29 23:55:18 -0700

    remove outdated setting of sources

--- viz.js
+++ viz.js
@@ -32,9 +32,3 @@ viz3btn.onclick = function(){
 };
 
 
-document.getElementById("viz1").src = "/Visualizations/viz1.html"
-document.getElementById("viz2").src = "/Visualizations/viz2.html"
-document.getElementById("viz2Covid").src = "/Visualizations/viz2Covid.html"
-document.getElementById("viz3Covid").src = "/Visualizations/viz3Covid.html"
-document.getElementById("viz4").src = "/Visualizations/viz4.html"
-document.getElementById("viz4Covid").src = "/Visualizations/viz4Covid.html"
